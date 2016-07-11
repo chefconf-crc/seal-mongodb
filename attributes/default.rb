237722f0d34904d5621419446a074a28ffefcb69
Cooper Simmons <cooper.simmons@gmail.com>
2016-07-11 21:44:13 +0000

    Moves creation of service user for mongodb into resources

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,16 +3,10 @@
 ## Attributes:: default
 ##
 
+## Commenting these out... they made my chef run break
 # Base attributes
-node['mongodb']['config']['dbpath'] = '/var/lib/mongodb'
-node['mongodb']['config']['logpath'] = '/var/log/mongodb/mongodb.log'
-node['mongodb']['config']['port'] = '27017'
-node['mongodb']['config']['bind_ip'] = 'localhost'
+#node['mongodb']['config']['dbpath'] = '/var/lib/mongodb'
+#node['mongodb']['config']['logpath'] = '/var/log/mongodb/mongodb.log'
+#node['mongodb']['config']['port'] = '27017'
+#node['mongodb']['config']['bind_ip'] = 'localhost'
 
-# User attributes
-default['seal_mongodb']['group'] = 'mongod'
-default['seal_mongodb']['gid'] = '1337'
-default['seal_mongodb']['user'] = 'mongod'
-default['seal_mongodb']['uid'] = '1337'
-default['seal_mongodb']['home'] = '/var/lib/mongo'
-default['seal_mongodb']['shell'] = '/bin/false'
