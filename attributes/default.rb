834a54ab8868e52a1c5c4580e30d4b50c4ceb190
David Bettinger <dave.bettinger@stelligent.com>
2016-07-11 19:36:33 +0000

    add base attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,13 @@
 ## Attributes:: default
 ##
 
+# Base attributes
+node['mongodb']['config']['dbpath'] = '/var/lib/mongodb'
+node['mongodb']['config']['logpath'] = '/var/log/mongodb/mongodb.log'
+node['mongodb']['config']['port'] = '27017'
+node['mongodb']['config']['bind_ip'] = 'localhost'
 
+# User attributes
 default['seal_mongodb']['group'] = 'mongod'
 default['seal_mongodb']['gid'] = '1337'
 default['seal_mongodb']['user'] = 'mongod'
